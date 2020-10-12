870f142ab295fd26c7bdbdf87a47b349482a98dc
bob cobey <rpcolbey@gmail.com>
2020-10-12 14:19:09 -0400

    include connection in try/catch

--- controller/src/insert.cpp
+++ controller/src/insert.cpp
@@ -11,18 +11,18 @@
  */
 void insert_reading(appConfig *config, const char *location, const char *device, const char *device_id, const char *sensor, int reading)
 {
-	// Create new PG connection
-	pqxx::connection connection{config->pg_connection};
-
 	// Mark insert with a timestamp
 	long int ts = static_cast<long int> (std::time(0));
 
-	// Prepare Reading
-	std::string sql = "INSERT INTO readings(location, device, device_id, sensor, ts, reading) VALUES ($1, $2, $3, $4, to_timestamp($5), $6)";
-	connection.prepare("readings_insert", sql);
-
 	try
 	{
+		// Create new PG connection
+		pqxx::connection connection{config->pg_connection};
+
+		// Prepare Reading
+		std::string sql = "INSERT INTO readings(location, device, device_id, sensor, ts, reading) VALUES ($1, $2, $3, $4, to_timestamp($5), $6)";
+		connection.prepare("readings_insert", sql);
+
 		// Perform transaction
 		pqxx::work transaction{connection};
 		cout << "DEBUG [insert] Prepare readings for location: " << location << ", device: " << device << ", device_id: " << device_id << ", sensor: " << sensor << ", ts: " << ts << ", reading: " << reading << endl;
